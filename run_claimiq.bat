6404bfcd9276fbdf0f10de929fbc573cff88bdf0
nirmshah23 <nirmshah@gmail.com>
2026-04-18 22:01:07 +0530

    updated bat file

--- run_claimiq.bat
+++ run_claimiq.bat
@@ -1,12 +1,38 @@
 @echo off
 
-REM Change drive to F:
+REM Change drive
 F:
 
-REM Change directory to the project folder
+REM Navigate to project folder
 cd "F:\SNU HC Products\ClaimIQ"
 
-REM Run uvicorn
-uvicorn main:app --reload
+echo ============================
+echo Pulling latest code from GitHub...
+echo ============================
+git pull
+
+REM Check if virtual environment exists
+if not exist .venv (
+    echo ============================
+    echo Creating virtual environment...
+    echo ============================
+    python -m venv .venv
+)
+
+echo ============================
+echo Activating virtual environment...
+echo ============================
+call .venv\Scripts\activate
+
+echo ============================
+echo Installing/updating requirements...
+echo ============================
+python.exe -m pip install --upgrade pip
+pip install -r requirements.txt
+
+echo ============================
+echo Starting application...
+echo ============================
+uvicorn main:app --port 8000
 
 pause
